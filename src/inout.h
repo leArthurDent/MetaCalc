28e3572abec572dfe27739083fa3d11b36850058
Sascha Manier <SaschaManier@posteo.de>
2018-05-27 21:24:56 +0200

    Added infrastructure for user defined metabolism. This included a rewrite of the metabolise function. An intelligent naming of the metabolites is still misssing.

--- src/inout.h
+++ src/inout.h
@@ -2,19 +2,19 @@
 #define INOUT_H_INCLUDED
 
 typedef struct{
-    char name[20];
-    int intFormula[10];
+    char name [25];
+    int intFormula [10];
     char charFormula [33];
     int nominalMass;
     double exactMass;
 }chemical;
 
-void chemscanf (int formula[10]);
+void parseFormula (char charFormula[33], int intFormula[10]);
 
-void assembleFormula(int intFormula[10], char charFormula[33]);
+void assembleFormula (int intFormula[10], char charFormula[33]);
 
-void displayTable(chemical *output, int size);
+void displayTable (chemical *output, int size);
 
-void exportTable(chemical *table, int size);
+void exportTable (chemical *table, int size);
 
 #endif
--- src/metabolism.c
+++ src/metabolism.c
@@ -3,207 +3,220 @@
     - metabolise (chemical *compound)
 */
 
+#include <stdio.h>
 #include <string.h>
 #include <metabolism.h>
 
 /*Function to calculate integer formulas of metabolites*/
-void metabolise(chemical *compound)
-{
-    /*Naming of the compounds*/
-    strcpy(compound[0].name, "Parent");
-    strcpy(compound[1].name, "oxo-");
-    strcpy(compound[2].name, "HO-");
-    strcpy(compound[3].name, "di-oxo-");
-    strcpy(compound[4].name, "oxo-HO-");
-    strcpy(compound[5].name, "di-HO-");
-    strcpy(compound[6].name, "tri-oxo-");
-    strcpy(compound[7].name, "di-oxo-HO-");
-    strcpy(compound[8].name, "oxo-di-HO-");
-    strcpy(compound[9].name, "tri-HO-");
-    strcpy(compound[10].name, "HO-Gluc");
-    strcpy(compound[11].name, "oxo-HO-Gluc");
-    strcpy(compound[12].name, "di-HO-Gluc");
-    strcpy(compound[13].name, "di-oxo-HO-Gluc");
-    strcpy(compound[14].name, "oxo-di-HO-Gluc");
-    strcpy(compound[15].name, "tri-HO-Gluc");
+void 
+metabolise (chemical *compound, int metabolism[2]) {
 
-    /*Alteration of the native formula for an oxo-metabolite*/
-    compound[1].intFormula[0] = compound[0].intFormula[0];
-    compound[1].intFormula[1] = compound[0].intFormula[1]-2;
-    compound[1].intFormula[2] = compound[0].intFormula[2];
-    compound[1].intFormula[3] = compound[0].intFormula[3];
-    compound[1].intFormula[4] = compound[0].intFormula[4];
-    compound[1].intFormula[5] = compound[0].intFormula[5];
-    compound[1].intFormula[6] = compound[0].intFormula[6];
-    compound[1].intFormula[7] = compound[0].intFormula[7]+1;
-    compound[1].intFormula[8] = compound[0].intFormula[8];
-    compound[1].intFormula[9] = compound[0].intFormula[9];
+	int i, j;
+	int compoundIndex = 0;
 
-    /*Alteration of the native formula for a hydroxy-metabolite*/
-    compound[2].intFormula[0] = compound[0].intFormula[0];
-    compound[2].intFormula[1] = compound[0].intFormula[1];
-    compound[2].intFormula[2] = compound[0].intFormula[2];
-    compound[2].intFormula[3] = compound[0].intFormula[3];
-    compound[2].intFormula[4] = compound[0].intFormula[4];
-    compound[2].intFormula[5] = compound[0].intFormula[5];
-    compound[2].intFormula[6] = compound[0].intFormula[6];
-    compound[2].intFormula[7] = compound[0].intFormula[7]+1;
-    compound[2].intFormula[8] = compound[0].intFormula[8];
-    compound[2].intFormula[9] = compound[0].intFormula[9];
+	/*char assembleName(int a, int b, int c) {
 
-    /*Alteration of the native formula for a di-oxo-metabolite*/
-    compound[3].intFormula[0] = compound[0].intFormula[0];
-    compound[3].intFormula[1] = compound[0].intFormula[1]-4;
-    compound[3].intFormula[2] = compound[0].intFormula[2];
-    compound[3].intFormula[3] = compound[0].intFormula[3];
-    compound[3].intFormula[4] = compound[0].intFormula[4];
-    compound[3].intFormula[5] = compound[0].intFormula[5];
-    compound[3].intFormula[6] = compound[0].intFormula[6];
-    compound[3].intFormula[7] = compound[0].intFormula[7]+2;
-    compound[3].intFormula[8] = compound[0].intFormula[8];
-    compound[3].intFormula[9] = compound[0].intFormula[9];
+		int a;
+		char name[20];
 
-    /*Alteration of the native formula for an oxo-hydroxy-metabolite*/
-    compound[4].intFormula[0] = compound[0].intFormula[0];
-    compound[4].intFormula[1] = compound[0].intFormula[1]-2;
-    compound[4].intFormula[2] = compound[0].intFormula[2];
-    compound[4].intFormula[3] = compound[0].intFormula[3];
-    compound[4].intFormula[4] = compound[0].intFormula[4];
-    compound[4].intFormula[5] = compound[0].intFormula[5];
-    compound[4].intFormula[6] = compound[0].intFormula[6];
-    compound[4].intFormula[7] = compound[0].intFormula[7]+2;
-    compound[4].intFormula[8] = compound[0].intFormula[8];
-    compound[4].intFormula[9] = compound[0].intFormula[9];
+		for(a = 0; a < 20; a++) {
+			
+    		name[a] = 'y';
+			
+		}
 
-    /*Alteration of the native formula for a di-hydroxy-metabolite*/
-    compound[5].intFormula[0] = compound[0].intFormula[0];
-    compound[5].intFormula[1] = compound[0].intFormula[1];
-    compound[5].intFormula[2] = compound[0].intFormula[2];
-    compound[5].intFormula[3] = compound[0].intFormula[3];
-    compound[5].intFormula[4] = compound[0].intFormula[4];
-    compound[5].intFormula[5] = compound[0].intFormula[5];
-    compound[5].intFormula[6] = compound[0].intFormula[6];
-    compound[5].intFormula[7] = compound[0].intFormula[7]+2;
-    compound[5].intFormula[8] = compound[0].intFormula[8];
-    compound[5].intFormula[9] = compound[0].intFormula[9];
+		
 
-    /*Alteration of the native formula for a tri-oxo-metabolite*/
-    compound[6].intFormula[0] = compound[0].intFormula[0];
-    compound[6].intFormula[1] = compound[0].intFormula[1]-6;
-    compound[6].intFormula[2] = compound[0].intFormula[2];
-    compound[6].intFormula[3] = compound[0].intFormula[3];
-    compound[6].intFormula[4] = compound[0].intFormula[4];
-    compound[6].intFormula[5] = compound[0].intFormula[5];
-    compound[6].intFormula[6] = compound[0].intFormula[6];
-    compound[6].intFormula[7] = compound[0].intFormula[7]+3;
-    compound[6].intFormula[8] = compound[0].intFormula[8];
-    compound[6].intFormula[9] = compound[0].intFormula[9];
+	}*/
 
-    /*Alteration of the native formula for a di-oxo-hydroxy-metabolite*/
-    compound[7].intFormula[0] = compound[0].intFormula[0];
-    compound[7].intFormula[1] = compound[0].intFormula[1]-4;
-    compound[7].intFormula[2] = compound[0].intFormula[2];
-    compound[7].intFormula[3] = compound[0].intFormula[3];
-    compound[7].intFormula[4] = compound[0].intFormula[4];
-    compound[7].intFormula[5] = compound[0].intFormula[5];
-    compound[7].intFormula[6] = compound[0].intFormula[6];
-    compound[7].intFormula[7] = compound[0].intFormula[7]+3;
-    compound[7].intFormula[8] = compound[0].intFormula[8];
-    compound[7].intFormula[9] = compound[0].intFormula[9];
+	/*Hydroxylation of parent compound*/
+    for (i = 1; i <= metabolism[0]; i++) {
 
-    /*Alteration of the native formula for an oxo-di-hydroxy-metabolite*/
-    compound[8].intFormula[0] = compound[0].intFormula[0];
-    compound[8].intFormula[1] = compound[0].intFormula[1]-2;
-    compound[8].intFormula[2] = compound[0].intFormula[2];
-    compound[8].intFormula[3] = compound[0].intFormula[3];
-    compound[8].intFormula[4] = compound[0].intFormula[4];
-    compound[8].intFormula[5] = compound[0].intFormula[5];
-    compound[8].intFormula[6] = compound[0].intFormula[6];
-    compound[8].intFormula[7] = compound[0].intFormula[7]+3;
-    compound[8].intFormula[8] = compound[0].intFormula[8];
-    compound[8].intFormula[9] = compound[0].intFormula[9];
+		compoundIndex++;
 
-    /*Alteration of the native formula for a tri-hydroxy-metabolite*/
-    compound[9].intFormula[0] = compound[0].intFormula[0];
-    compound[9].intFormula[1] = compound[0].intFormula[1];
-    compound[9].intFormula[2] = compound[0].intFormula[2];
-    compound[9].intFormula[3] = compound[0].intFormula[3];
-    compound[9].intFormula[4] = compound[0].intFormula[4];
-    compound[9].intFormula[5] = compound[0].intFormula[5];
-    compound[9].intFormula[6] = compound[0].intFormula[6];
-    compound[9].intFormula[7] = compound[0].intFormula[7]+3;
-    compound[9].intFormula[8] = compound[0].intFormula[8];
-    compound[9].intFormula[9] = compound[0].intFormula[9];
+		compound[compoundIndex].intFormula[0] = compound[0].intFormula[0];
+		compound[compoundIndex].intFormula[1] = compound[0].intFormula[1];
+		compound[compoundIndex].intFormula[2] = compound[0].intFormula[2];
+		compound[compoundIndex].intFormula[3] = compound[0].intFormula[3];
+		compound[compoundIndex].intFormula[4] = compound[0].intFormula[4];
+		compound[compoundIndex].intFormula[5] = compound[0].intFormula[5];
+		compound[compoundIndex].intFormula[6] = compound[0].intFormula[6];
+		compound[compoundIndex].intFormula[7] = compound[0].intFormula[7] +i;
+		compound[compoundIndex].intFormula[8] = compound[0].intFormula[8];
+		compound[compoundIndex].intFormula[9] = compound[0].intFormula[9];
 
-    /*Alteration of the native formula for a hydroxy-glucuronide-metabolite*/
-    compound[10].intFormula[0] = compound[0].intFormula[0]+6;
-    compound[10].intFormula[1] = compound[0].intFormula[1]+8;
-    compound[10].intFormula[2] = compound[0].intFormula[2];
-    compound[10].intFormula[3] = compound[0].intFormula[3];
-    compound[10].intFormula[4] = compound[0].intFormula[4];
-    compound[10].intFormula[5] = compound[0].intFormula[5];
-    compound[10].intFormula[6] = compound[0].intFormula[6];
-    compound[10].intFormula[7] = compound[0].intFormula[7]+7;
-    compound[10].intFormula[8] = compound[0].intFormula[8];
-    compound[10].intFormula[9] = compound[0].intFormula[9];
+	}
 
-    /*Alteration of the native formula for an oxo-hydroxy-glucuronide-metabolite*/
-    compound[11].intFormula[0] = compound[0].intFormula[0]+6;
-    compound[11].intFormula[1] = compound[0].intFormula[1]+6;
-    compound[11].intFormula[2] = compound[0].intFormula[2];
-    compound[11].intFormula[3] = compound[0].intFormula[3];
-    compound[11].intFormula[4] = compound[0].intFormula[4];
-    compound[11].intFormula[5] = compound[0].intFormula[5];
-    compound[11].intFormula[6] = compound[0].intFormula[6];
-    compound[11].intFormula[7] = compound[0].intFormula[7]+8;
-    compound[11].intFormula[8] = compound[0].intFormula[8];
-    compound[11].intFormula[9] = compound[0].intFormula[9];
+    /*Formation of oxo-metabolites*/
+	for (i = 1; i <= metabolism[1]; i++) {
 
-    /*Alteration of the native formula for a di-hydroxy-glucuronide-metabolite*/
-    compound[12].intFormula[0] = compound[0].intFormula[0]+6;
-    compound[12].intFormula[1] = compound[0].intFormula[1]+8;
-    compound[12].intFormula[2] = compound[0].intFormula[2];
-    compound[12].intFormula[3] = compound[0].intFormula[3];
-    compound[12].intFormula[4] = compound[0].intFormula[4];
-    compound[12].intFormula[5] = compound[0].intFormula[5];
-    compound[12].intFormula[6] = compound[0].intFormula[6];
-    compound[12].intFormula[7] = compound[0].intFormula[7]+8;
-    compound[12].intFormula[8] = compound[0].intFormula[8];
-    compound[12].intFormula[9] = compound[0].intFormula[9];
+		compoundIndex++;
 
-    /*Alteration of the native formula for a di-oxo-hydroxy-glucuronide-metabolite*/
-    compound[13].intFormula[0] = compound[0].intFormula[0]+6;
-    compound[13].intFormula[1] = compound[0].intFormula[1]+4;
-    compound[13].intFormula[2] = compound[0].intFormula[2];
-    compound[13].intFormula[3] = compound[0].intFormula[3];
-    compound[13].intFormula[4] = compound[0].intFormula[4];
-    compound[13].intFormula[5] = compound[0].intFormula[5];
-    compound[13].intFormula[6] = compound[0].intFormula[6];
-    compound[13].intFormula[7] = compound[0].intFormula[7]+9;
-    compound[13].intFormula[8] = compound[0].intFormula[8];
-    compound[13].intFormula[9] = compound[0].intFormula[9];
+		compound[compoundIndex].intFormula[0] = compound[0].intFormula[0];
+		compound[compoundIndex].intFormula[1] = compound[0].intFormula[1] -2 *i;
+		compound[compoundIndex].intFormula[2] = compound[0].intFormula[2];
+		compound[compoundIndex].intFormula[3] = compound[0].intFormula[3];
+		compound[compoundIndex].intFormula[4] = compound[0].intFormula[4];
+		compound[compoundIndex].intFormula[5] = compound[0].intFormula[5];
+		compound[compoundIndex].intFormula[6] = compound[0].intFormula[6];
+		compound[compoundIndex].intFormula[7] = compound[0].intFormula[7] +i;
+		compound[compoundIndex].intFormula[8] = compound[0].intFormula[8];
+		compound[compoundIndex].intFormula[9] = compound[0].intFormula[9];
 
-    /*Alteration of the native formula for an oxo-di-hydroxy-glucuronide-metabolite*/
-    compound[14].intFormula[0] = compound[0].intFormula[0]+6;
-    compound[14].intFormula[1] = compound[0].intFormula[1]+6;
-    compound[14].intFormula[2] = compound[0].intFormula[2];
-    compound[14].intFormula[3] = compound[0].intFormula[3];
-    compound[14].intFormula[4] = compound[0].intFormula[4];
-    compound[14].intFormula[5] = compound[0].intFormula[5];
-    compound[14].intFormula[6] = compound[0].intFormula[6];
-    compound[14].intFormula[7] = compound[0].intFormula[7]+9;
-    compound[14].intFormula[8] = compound[0].intFormula[8];
-    compound[14].intFormula[9] = compound[0].intFormula[9];
+	}
 
-    /*Alteration of the native formula for a tri-hydroxy-glucuronide-metabolite*/
-    compound[15].intFormula[0] = compound[0].intFormula[0]+6;
-    compound[15].intFormula[1] = compound[0].intFormula[1]+8;
-    compound[15].intFormula[2] = compound[0].intFormula[2];
-    compound[15].intFormula[3] = compound[0].intFormula[3];
-    compound[15].intFormula[4] = compound[0].intFormula[4];
-    compound[15].intFormula[5] = compound[0].intFormula[5];
-    compound[15].intFormula[6] = compound[0].intFormula[6];
-    compound[15].intFormula[7] = compound[0].intFormula[7]+9;
-    compound[15].intFormula[8] = compound[0].intFormula[8];
-    compound[15].intFormula[9] = compound[0].intFormula[9];
+    /*Formation of combinations of hydroxylations and oxo formations*/
+	for (i = 1; i <= metabolism[0]; i++) {
+
+		for (j = 1; j <= metabolism[1]; j++) {
+
+			compoundIndex++;
+
+			compound[compoundIndex].intFormula[0] = compound[0].intFormula[0];
+			compound[compoundIndex].intFormula[1] = compound[0].intFormula[1]-2*j;
+			compound[compoundIndex].intFormula[2] = compound[0].intFormula[2];
+			compound[compoundIndex].intFormula[3] = compound[0].intFormula[3];
+			compound[compoundIndex].intFormula[4] = compound[0].intFormula[4];
+			compound[compoundIndex].intFormula[5] = compound[0].intFormula[5];
+			compound[compoundIndex].intFormula[6] = compound[0].intFormula[6];
+			compound[compoundIndex].intFormula[7] = compound[0].intFormula[7] +i +j;
+			compound[compoundIndex].intFormula[8] = compound[0].intFormula[8];
+			compound[compoundIndex].intFormula[9] = compound[0].intFormula[9];
+
+		}
+
+	}
+
+    /*Glucuronidation of every metabolite that was merely hydroxylated*/
+	for (i = 1; i <= metabolism[0]; i++) {
+
+		compoundIndex++;
+
+		compound[compoundIndex].intFormula[0] = compound[0].intFormula[0] +6;
+		compound[compoundIndex].intFormula[1] = compound[0].intFormula[1] +8;
+		compound[compoundIndex].intFormula[2] = compound[0].intFormula[2];
+		compound[compoundIndex].intFormula[3] = compound[0].intFormula[3];
+		compound[compoundIndex].intFormula[4] = compound[0].intFormula[4];
+		compound[compoundIndex].intFormula[5] = compound[0].intFormula[5];
+		compound[compoundIndex].intFormula[6] = compound[0].intFormula[6];
+		compound[compoundIndex].intFormula[7] = compound[0].intFormula[7] +i +6;
+		compound[compoundIndex].intFormula[8] = compound[0].intFormula[8];
+		compound[compoundIndex].intFormula[9] = compound[0].intFormula[9];
+
+	}
+
+	/*Glucuronidation of combinations of hydroxy and oxo metabolites*/
+	for (i = 1; i <= metabolism[0]; i++) {
+
+		for (j = 1; j <= metabolism[1]; j++) {
+
+			compoundIndex++;
+
+			compound[compoundIndex].intFormula[0] = compound[0].intFormula[0] +6;
+			compound[compoundIndex].intFormula[1] = compound[0].intFormula[1] +8 -2*j;
+			compound[compoundIndex].intFormula[2] = compound[0].intFormula[2];
+			compound[compoundIndex].intFormula[3] = compound[0].intFormula[3];
+			compound[compoundIndex].intFormula[4] = compound[0].intFormula[4];
+			compound[compoundIndex].intFormula[5] = compound[0].intFormula[5];
+			compound[compoundIndex].intFormula[6] = compound[0].intFormula[6];
+			compound[compoundIndex].intFormula[7] = compound[0].intFormula[7] +i +j +6;
+			compound[compoundIndex].intFormula[8] = compound[0].intFormula[8];
+			compound[compoundIndex].intFormula[9] = compound[0].intFormula[9];
+
+		}
+
+	}
+
+	strcpy (compound[0].name, "Parent");
+	strcpy (compound[1].name, "-M (HO-)");
+	strcpy (compound[2].name, "-M (di-HO-)");
+	strcpy (compound[3].name, "-M (tri-HO-)");
+	strcpy (compound[4].name, "-M (oxo-)");
+	strcpy (compound[5].name, "-M (di-oxo-)");                                
+	strcpy (compound[6].name, "-M (oxo-HO-)");
+	strcpy (compound[7].name, "-M (di-oxo-HO-)");
+	strcpy (compound[8].name, "-M (oxo-di-HO-)");
+	strcpy (compound[9].name, "-M (di-oxo-di-HO-)");
+	strcpy (compound[10].name, "-M (oxo-tri-HO-)");
+	strcpy (compound[11].name, "-M (tri-oxo-tri-HO-)");
+	strcpy (compound[12].name, "-M (HO-) Gluc");
+	strcpy (compound[13].name, "-M (di-HO-) Gluc");
+	strcpy (compound[14].name, "-M (tri-HO-) Gluc");
+	strcpy (compound[15].name, "-M (oxo-HO-) Gluc");
+	strcpy (compound[16].name, "-M (di-oxo-HO-) Gluc");
+	strcpy (compound[17].name, "-M (oxo-di-HO-) Gluc");
+	strcpy (compound[18].name, "-M (di-oxo-di-HO-) Gluc");
+	strcpy (compound[19].name, "-M (oxo-tri-HO-) Gluc");
+	strcpy (compound[20].name, "-M (tri-oxo-tri-HO-) Gluc");
+
+
+	/*Naming of the compounds*/
+	/*strcpy(compound[0].name, "Parent");
+	
+	for (i = 1; i <= compoundIndex; i++) {
+
+		int diffFormula[10];
+		int a, b, c, d;
+
+		for(a = 0; a < 20; a++) {
+			
+    		compound[i].name[a] = 'x';
+			
+		}
+
+		for (j = 0; j < 10; j++) {
+
+			diffFormula[0] = compound[i].intFormula[j] - compound[0].intFormula[j];
+
+		}
+
+		printf("%d %d %d %d %d %d %d %d %d %d\n", diffFormula[0], diffFormula[1], diffFormula[2], diffFormula[3] , diffFormula[4], diffFormula[5], diffFormula[6], diffFormula[7], diffFormula[8], diffFormula[9]);
+
+		for (a = 0; a < 10; a++) {
+
+			for (b = 0; b < 10; b++) {
+
+				for (c = 0; c < 2; c++) {
+
+					int nameFormula[10], compFormula[10], sum;
+
+					nameFormula[0] = compound[0].intFormula[0] +6*c;
+					nameFormula[1] = compound[0].intFormula[1] +8*c -2*b;
+					nameFormula[2] = compound[0].intFormula[2];
+					nameFormula[3] = compound[0].intFormula[3];
+					nameFormula[4] = compound[0].intFormula[4];
+					nameFormula[5] = compound[0].intFormula[5];
+					nameFormula[6] = compound[0].intFormula[6];
+					nameFormula[7] = compound[0].intFormula[7] +a +b;
+					nameFormula[8] = compound[0].intFormula[8];
+					nameFormula[9] = compound[0].intFormula[9];
+
+					for (d = 0; d < 10; d++) {
+
+						compFormula[d] = diffFormula[d] - nameFormula[d];
+
+					}
+					
+					for (d = 0; d < 10; d++) {
+
+						sum += compFormula[d];
+
+					}
+
+					if (sum == 0) {
+
+					/*	compound[i].charFormula = assembleName (a, b, c);
+
+					}
+					
+				}
+
+			}
+			
+			
+		}
+		
+
+	}*/
+
 }
