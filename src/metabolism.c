7f1a4728c867f8c35b1f8da9a6271ca5ba8d78f9
Sascha Manier <SaschaManier@posteo.de>
2018-05-28 22:16:17 +0200

    Dynamic naming of metabolites possible!

--- src/metabolism.c
+++ src/metabolism.c
@@ -4,30 +4,113 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <metabolism.h>
+#include "metabolism.h"
+
+void
+assembleName(char name[25], int hydroxy, int oxo, int gluc) {
+
+    strcpy(name, "-M (");
+
+    switch(oxo) {
+
+    case 2: strcat(name, "di-");
+            break;
+
+    case 3: strcat(name, "tri-");
+            break;
+
+    case 4: strcat(name, "quattro-");
+            break;
+
+    case 5: strcat(name, "penta-");
+            break;
+
+    case 6: strcat(name, "hexa-");
+            break;
+
+    case 7: strcat(name, "hepta-");
+            break;
+
+    case 8: strcat(name, "octa-");
+            break;
+
+    case 9: strcat(name, "nona-");
+            break;
+
+    case 10: strcat(name, "deca-");
+             break;
+
+    }
+
+    if (oxo > 0) {
+
+        strcat(name, "oxo-");
+
+    }
+
+
+    switch(hydroxy) {
+
+    case 2: strcat(name, "di-");
+            break;
+
+    case 3: strcat(name, "tri-");
+            break;
+
+    case 4: strcat(name, "quattro-");
+            break;
+
+    case 5: strcat(name, "penta-");
+            break;
+
+    case 6: strcat(name, "hexa-");
+            break;
+
+    case 7: strcat(name, "hepta-");
+            break;
+
+    case 8: strcat(name, "octa-");
+            break;
+
+    case 9: strcat(name, "nona-");
+            break;
+
+    case 10: strcat(name, "deca-");
+             break;
+
+    }
+
+    if (hydroxy > 0) {
+
+        strcat(name, "HO-");
+
+    }
+
+    if (gluc == 1) {
+
+        strcat(name, ") Gluc");
+
+    } else {
+
+        strcat(name, ")");
+
+    }
+
+
+
+}
 
 /*Function to calculate integer formulas of metabolites*/
 void 
 metabolise (chemical *compound, int metabolism[2]) {
 
+
+
 	int i, j;
 	int compoundIndex = 0;
 
-	/*char assembleName(int a, int b, int c) {
-
-		int a;
-		char name[20];
-
-		for(a = 0; a < 20; a++) {
-			
-    		name[a] = 'y';
-			
-		}
-
-		
-
-	}*/
 
 	/*Hydroxylation of parent compound*/
     for (i = 1; i <= metabolism[0]; i++) {
@@ -93,7 +176,7 @@ metabolise (chemical *compound, int metabolism[2]) {
 		compoundIndex++;
 
 		compound[compoundIndex].intFormula[0] = compound[0].intFormula[0] +6;
-		compound[compoundIndex].intFormula[1] = compound[0].intFormula[1] +8;
+		compound[compoundIndex].intFormula[1] = compound[0].intFormula[1] +10;
 		compound[compoundIndex].intFormula[2] = compound[0].intFormula[2];
 		compound[compoundIndex].intFormula[3] = compound[0].intFormula[3];
 		compound[compoundIndex].intFormula[4] = compound[0].intFormula[4];
@@ -113,7 +196,7 @@ metabolise (chemical *compound, int metabolism[2]) {
 			compoundIndex++;
 
 			compound[compoundIndex].intFormula[0] = compound[0].intFormula[0] +6;
-			compound[compoundIndex].intFormula[1] = compound[0].intFormula[1] +8 -2*j;
+			compound[compoundIndex].intFormula[1] = compound[0].intFormula[1] +10 -2*j;
 			compound[compoundIndex].intFormula[2] = compound[0].intFormula[2];
 			compound[compoundIndex].intFormula[3] = compound[0].intFormula[3];
 			compound[compoundIndex].intFormula[4] = compound[0].intFormula[4];
@@ -127,85 +210,54 @@ metabolise (chemical *compound, int metabolism[2]) {
 
 	}
 
-	strcpy (compound[0].name, "Parent");
-	strcpy (compound[1].name, "-M (HO-)");
-	strcpy (compound[2].name, "-M (di-HO-)");
-	strcpy (compound[3].name, "-M (tri-HO-)");
-	strcpy (compound[4].name, "-M (oxo-)");
-	strcpy (compound[5].name, "-M (di-oxo-)");                                
-	strcpy (compound[6].name, "-M (oxo-HO-)");
-	strcpy (compound[7].name, "-M (di-oxo-HO-)");
-	strcpy (compound[8].name, "-M (oxo-di-HO-)");
-	strcpy (compound[9].name, "-M (di-oxo-di-HO-)");
-	strcpy (compound[10].name, "-M (oxo-tri-HO-)");
-	strcpy (compound[11].name, "-M (tri-oxo-tri-HO-)");
-	strcpy (compound[12].name, "-M (HO-) Gluc");
-	strcpy (compound[13].name, "-M (di-HO-) Gluc");
-	strcpy (compound[14].name, "-M (tri-HO-) Gluc");
-	strcpy (compound[15].name, "-M (oxo-HO-) Gluc");
-	strcpy (compound[16].name, "-M (di-oxo-HO-) Gluc");
-	strcpy (compound[17].name, "-M (oxo-di-HO-) Gluc");
-	strcpy (compound[18].name, "-M (di-oxo-di-HO-) Gluc");
-	strcpy (compound[19].name, "-M (oxo-tri-HO-) Gluc");
-	strcpy (compound[20].name, "-M (tri-oxo-tri-HO-) Gluc");
-
-
 	/*Naming of the compounds*/
-	/*strcpy(compound[0].name, "Parent");
+	strcpy(compound[0].name, "Parent");
 	
 	for (i = 1; i <= compoundIndex; i++) {
 
 		int diffFormula[10];
-		int a, b, c, d;
+		int hydroxy, oxo, gluc = 0;
 
-		for(a = 0; a < 20; a++) {
+		for(j = 0; j < sizeof(compound->name); j++) {
 			
-    		compound[i].name[a] = 'x';
+    		compound[i].name[j] = 'x';
 			
 		}
 
 		for (j = 0; j < 10; j++) {
 
-			diffFormula[0] = compound[i].intFormula[j] - compound[0].intFormula[j];
+			diffFormula[j] = compound[i].intFormula[j] - compound[0].intFormula[j];
 
 		}
 
-		printf("%d %d %d %d %d %d %d %d %d %d\n", diffFormula[0], diffFormula[1], diffFormula[2], diffFormula[3] , diffFormula[4], diffFormula[5], diffFormula[6], diffFormula[7], diffFormula[8], diffFormula[9]);
+		for (gluc = 0; gluc <= 1; gluc++) {
 
-		for (a = 0; a < 10; a++) {
+			for (oxo = 0; oxo <= metabolism[1]; oxo++) {
 
-			for (b = 0; b < 10; b++) {
+				for (hydroxy = 0; hydroxy <= metabolism[0]; hydroxy++) {
 
-				for (c = 0; c < 2; c++) {
+					int compFormula[10], sum = 0;
 
-					int nameFormula[10], compFormula[10], sum;
+					compFormula[0] = diffFormula[0] - 6*gluc;
+					compFormula[1] = diffFormula[1] - 10*gluc +2*oxo;
+					compFormula[2] = diffFormula[2];
+					compFormula[3] = diffFormula[3];
+					compFormula[4] = diffFormula[4];
+					compFormula[5] = diffFormula[5];
+					compFormula[6] = diffFormula[6];
+					compFormula[7] = diffFormula[7] - hydroxy -oxo -6*gluc;
+					compFormula[8] = diffFormula[8];
+					compFormula[9] = diffFormula[9];
 
-					nameFormula[0] = compound[0].intFormula[0] +6*c;
-					nameFormula[1] = compound[0].intFormula[1] +8*c -2*b;
-					nameFormula[2] = compound[0].intFormula[2];
-					nameFormula[3] = compound[0].intFormula[3];
-					nameFormula[4] = compound[0].intFormula[4];
-					nameFormula[5] = compound[0].intFormula[5];
-					nameFormula[6] = compound[0].intFormula[6];
-					nameFormula[7] = compound[0].intFormula[7] +a +b;
-					nameFormula[8] = compound[0].intFormula[8];
-					nameFormula[9] = compound[0].intFormula[9];
+					for (j = 0; j < 10; j++) {
 
-					for (d = 0; d < 10; d++) {
-
-						compFormula[d] = diffFormula[d] - nameFormula[d];
-
-					}
-					
-					for (d = 0; d < 10; d++) {
-
-						sum += compFormula[d];
+						sum += abs(compFormula[j]);
 
 					}
 
 					if (sum == 0) {
 
-					/*	compound[i].charFormula = assembleName (a, b, c);
+						assembleName (compound[i].name, hydroxy, oxo, gluc);
 
 					}
 					
@@ -213,10 +265,8 @@ metabolise (chemical *compound, int metabolism[2]) {
 
 			}
 			
-			
 		}
-		
 
-	}*/
+	}
 
 }
